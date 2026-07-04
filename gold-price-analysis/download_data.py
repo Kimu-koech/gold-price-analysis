4d7e334c619d22342dd698f21be654a838c81e26
kimutai koech <koechkim.kelvin@gmail.com>
2026-07-04 23:10:13 +0300

    Fixing the dataset imported from yfinance correctly

--- gold-price-analysis/download_data.py
+++ gold-price-analysis/download_data.py
@@ -4,4 +4,9 @@ gold=yf.download(
     start="2016-01-01",
     end="2026-07-01"
 )
-gold.to_csv("data/raw/gold_prices.csv")
+
+if gold.columns.nlevels>1:
+    gold.columns=gold.columns.get_level_values(0)
+gold.reset_index(inplace=True)
+gold.to_csv("data/raw/gold_prices.csv",index=False)
+print("Data updated successfully")
